e824222e5523c79dc3f895e6d72c8c9a11248c0d
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-03-17 22:12:45 +0530

    Added Interest calculator

---
+++ ci&si.py
@@ -0,0 +1,24 @@
+class interCalc:
+    def __init__(self, principal, rate, time):
+        self.principal = principal
+        self.rate = rate
+        self.time = time
+
+
+    @property
+    def compInter(self):
+        return (self.principal * self.rate * self.time)/100
+    
+    @property
+    def simpInter(self):
+        return (self.principal * (1 + self.rate/100)** self.time - self.principal)
+    
+calc = interCalc(1000, 5, 2)  
+
+print("Simple Interest:", calc.simpInter)   # 1000 * 5 * 2 / 100 = 100
+print("Compound Interest:", calc.compInter) 
+
+calc.principal = 5000
+
+print("Updated Simple Interest: ", calc.simpInter)
+print("Updated Compound Interest: ", calc.compInter)
